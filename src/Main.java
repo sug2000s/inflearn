03b9618e1d3914fe3c4dea693cad909d0eebe3e2
CNS\sug2000s <sug2000s@gmail.com>
2021-08-14 23:37:06 +0900

    1차 문제

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(String str, char t){
+        int answer =0;
+        str = str.toUpperCase();
+        t=Character.toUpperCase(t);
+
+       /* for(int i=0; i<str.length();i++){
+            if(str.charAt(i) == t) answer++;
+        }*/
+
+        for(char x : str.toCharArray()){
+            if(x == t) answer++;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+        char c = in.next().charAt(0);
+        System.out.println(T.solution(str, c));
+
+    }
+}

26eb5015fc06d2d5617a0a9ff0288f5d72d4e106
CNS\sug2000s <sug2000s@gmail.com>
2022-06-06 21:39:06 +0900

    8. 팰린드롬(replaceAll 정규식이용)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-    public String solution(String str){ 
-        String answer = "";
+    public String solution(String s){
+        String answer = "NO";
 
-        for(int i = 0; i<str.length();i++)
-        {
-            if(str.indexOf(str.charAt(i))==i) answer +=str.charAt(i);//중복을 찾는 방법 외우면 좋음.
+        s=s.toUpperCase().replaceAll("[^A-Z]]","");
+        String tmp = new StringBuilder(s).reverse().toString();
+        if(s.equals(tmp)) answer="YES";
 
-        }
 
         return answer;
     }
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb =new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         System.out.println(T.solution(str));
 
     }

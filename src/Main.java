b9ad240dc883b5d56e8794f2a0b5d475a6423014
CNS\sug2000s <sug2000s@gmail.com>
2022-09-25 19:37:14 +0900

    7-4. 피보나치 재귀_220925

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,24 @@
-import java.lang.reflect.Array;
+
 import java.util.*;
 class Main {
-    public int solution(int n, int m, int[] arr){
-        int answer=0;
-        Arrays.sort(arr);
-        int lt=0,rt=n-1;
-        while(lt<=rt){
-            int mid = (lt+rt)/2;
-            int midNum = arr[mid];
-            if(midNum > m){
-                rt = mid-1;
-            }else if(midNum<m){
-                lt = mid+1;
-            }else if(midNum == m){
-                answer = mid+1;
-                break;
-
-            }
-
+    static int[] fibo;
+    public int DFS(int n){
+        if(fibo[n]>0){
+            return fibo[n];
         }
-        System.out.println(Arrays.toString(arr));
 
-        return answer;
+        if(n==1 || n==2){
+            return fibo[n] = 1;
+        }else{
+            return fibo[n] = DFS(n-1) + DFS(n-2);
+        }
+
     }
     public static void main(String[] args){
         Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int m=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        System.out.println(T.solution(n, m, arr));
+        int n=45;
+        fibo = new int[n+1];
+        T.DFS(n);
+        for(int i=1; i<=n; i++) System.out.print(fibo[i]+" ");
     }
 }
-
-/*
-8 32
-23 87 65 12 57 32 99 817
-*/

435ba51e8bbe4106ecd0b57ff6b8ee60ed454be5
CNS\sug2000s <sug2000s@gmail.com>
2022-09-25 15:38:30 +0900

    6-4. Least Recently Used(임시저장)

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,35 @@
 import java.util.*;
-
 class Main {
-    public int[] solution_selectionSorting(int n, int[] arr){
-        //1. 선택 정열
-        for(int i=0; i<arr.length;i++){
-            for(int j=i+1; j<arr.length;j++){
-                int selNum = arr[i];
-                int movNum = arr[j];
-                if(selNum > movNum){
-                    arr[j] = selNum;
-                    arr[i] = movNum;
+    public int[] solution(int size, int n, int[] arr){
+        int[] cache=new int[size];
+        System.out.println(Arrays.toString(arr));
+        for(int i=0; i<arr.length-1;i++){
 
-                }
-            }
-        }
-        return arr;
-    }
-
-    public int[] solution_bubbleSorting(int n, int[] arr){
-        //2. 버블 정열
-        for(int i=0; i<arr.length;i++){
-            for(int j=i+1; j<arr.length;j++){
-                int prevNum = arr[j-1];
-                int nextNum = arr[j];
-                if(prevNum > nextNum){
-                    arr[j] = prevNum;
-                    arr[j-1] = nextNum;
-                }
-            }
-
-        }
-
-        return arr;
-    }
-
-    public int[] solution_insertSorting(int n, int[] arr){
-        //3. 삽입 정열
-        for(int i=1;i<arr.length;i++){
-
-            for(int j=i; j>0; j--){
-                int prevNum = arr[j-1];
-                int currNum = arr[j];
-                if(prevNum > currNum){
-                    arr[j-1] = currNum;
-                    arr[j] = prevNum;
-                }
+            for(int j=0; j<cache.length;j++){
+                int newNum = arr[i];
+                int curNum = cache[j];
+                int nextNum = cache[j+1];
 
             }
 
-        }
-        return arr;
-    }
 
+
+
+        }
+
+        return cache;
+    }
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
+        int s=kb.nextInt();
         int n=kb.nextInt();
         int[] arr=new int[n];
         for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        for(int x : T.solution_insertSorting(n, arr)) System.out.print(x+" ");
+        for(int x : T.solution(s, n, arr)) System.out.print(x+" ");
     }
 }
 /*
-6
-13 5 11 7 23 15
+5 9
+1 2 3 2 6 2 3 5 7
  */

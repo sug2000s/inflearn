8ea094af6e5c5f6b40134becb60a1cef35482409
CNS\sug2000s <sug2000s@gmail.com>
2022-09-25 16:31:56 +0900

    6-7. 좌표 정렬_220925

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,49 @@
 import java.util.*;
+class Point implements Comparable<Point>{
+
+    public int x,y;
+
+    Point(int x, int y){
+        this.x = x;
+        this.y= y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x == o.x){
+            return this.y-o.y;
+        }else {
+            return o.x - this.x;
+        }
+    }
+}
+
 class Main {
-    public int[] solution(int size, int n, int[] arr){
-        int[] cache=new int[size];
-        System.out.println(Arrays.toString(arr));
-        for(int i=0; i<arr.length-1;i++){
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
 
-            for(int j=0; j<cache.length;j++){
-                int newNum = arr[i];
-                int curNum = cache[j];
-                int nextNum = cache[j+1];
-
-            }
-
-
-
-
+        ArrayList<Point> arr = new ArrayList<Point>();
+        for(int i=0; i<n; i++){
+            int x=kb.nextInt();
+            int y=kb.nextInt();
+            arr.add(new Point(x,y));
         }
 
-        return cache;
-    }
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int s=kb.nextInt();
-        int n=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        for(int x : T.solution(s, n, arr)) System.out.print(x+" ");
+        Collections.sort(arr);
+
+        for(Point o : arr) System.out.println(o.x + ", " + o.y);
+
     }
 }
 /*
-5 9
-1 2 3 2 6 2 3 5 7
+5
+2 7
+1 3
+1 2
+2 5
+3 6
+
+
+
  */

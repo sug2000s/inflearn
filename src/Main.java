4ad8d346e7412ba5341c7fdf2caf99b54aa5df7f
CNS\sug2000s <sug2000s@gmail.com>
2022-09-25 20:31:56 +0900

    7-6. 부분집합 구하기(DFS)_220925

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,32 @@
 import java.util.*;
-class Node{
-    int data;
-    Node lt, rt;
-    public Node(int val){
-        data = val;
-        lt=rt=null;
-    }
-}
-public class Main{
-    Node root;
-    public void DFS(Node root){
-        if(root==null){
+class Main {
+    static int[] chk;
+    static int n;
+    public void DFS(int L){
+        if(L==n+1){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i < chk.length-1; i++){
+                if(chk[i] > 0 ){
+                    sb.append(Integer.toString(i));
+                }
+            }
+            System.out.println(sb.toString());
             return;
         }else{
-           // System.out.println(root.data);
-            DFS(root.lt);
-            System.out.println(root.data);
-            DFS(root.rt);
-            //
+            chk[L] = 1;
+            DFS(L+1);
+            chk[L] = 0;
+            DFS(L+1);
         }
 
     }
 
-    public static void main(String args[]) {
-        Main tree=new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
+    public static void main(String[] args){
+        Main T = new Main();
+        n=8;
+        chk = new int[n+2];
 
-        tree.DFS(tree.root);
+        T.DFS(1);
 
     }
-} 
+}

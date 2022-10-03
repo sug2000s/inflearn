743e2fe6a2b7573fbad944f57c292282bf10bce0
CNS\sug2000s <sug2000s@gmail.com>
2022-10-03 14:26:42 +0900

    8-1. 합이 같은 부분집합_221003

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,36 @@
 import java.util.*;
-class Main {
-    static int n, m, answer=0;
-    static int[][] graph;
-    static int[] ch;
-    public void DFS(int v){
-        Queue<Integer> Q = new LinkedList<Integer>();
-        Q.offer(v);
-        while(!Q.isEmpty()){
-            Q.poll();
-            for(int i=1; i<graph.length; i++){
-                for(int j=1; j<graph[i].length;j++){
-                    int nextnum = graph[i][j];
-                    if(nextnum>0)Q.offer(j);
-                }
+class Main{
+    static String answer="NO";
+    static int n, total=0;
+    boolean flag=false;
+    public void DFS(int L, int sum, int[] arr){
+        if(L==n){
+            if(sum == total/2){
+                answer = "YES";
             }
-
+        }else{
+            sum = sum + arr[L];
+            DFS(L+1, sum, arr);
+            sum = sum - arr[L];
+            DFS(L+1, sum, arr);
 
         }
 
     }
-
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         n=kb.nextInt();
-        m=kb.nextInt();
-        graph=new int[n+1][n+1];
-        ch=new int[n+1];
-        for(int i=0; i<m; i++){
-            int a=kb.nextInt();
-            int b=kb.nextInt();
-            graph[a][b]=1;
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+            total+=arr[i];
         }
-        ch[1]=1;
-        T.DFS(1);
+        T.DFS(0, 0, arr);
         System.out.println(answer);
     }
 }
 /*
-
-5 9
-1 2
-1 3
-1 4
-2 1
-2 3
-2 5
-3 4
-4 2
-4 5
+6
+1 3 5 6 7 10
  */

a0b33070c600cb005b15388a7ebdc1f59e5cebcb
CNS\sug2000s <sug2000s@gmail.com>
2022-10-03 21:33:01 +0900

    8-7. 조합수(메모이제이션)_다시 복습 필요_221003

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,25 @@
 import java.util.*;
 class Main{
-    static int[] pm, ch, arr;
-    static int n, m;
-    public void DFS(int L){
-        if(L==m){
-            if(pm[0]==pm[1]){
-                return;
-            }else{
-                System.out.println(Arrays.toString(pm));
-            }
-
+    int[][] dy=new int[35][35];
+    public int DFS(int n, int r){
+        if(dy[n][r]>0)return dy[n][r];
+        if(n==r || r==0){
+            return 1;
         }else{
-            for(int i=1; i<=n;i++){
-                pm[L] = arr[i-1];
-                DFS(L+1);
-            }
+            return dy[n][r] = DFS(n-1,r-1) + DFS(n-1,r);
         }
-
     }
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        n=kb.nextInt();
-        m=kb.nextInt();
-        arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        ch=new int[n];
-        pm=new int[m];
-        T.DFS(0);
+        int n=kb.nextInt();
+        int r=kb.nextInt();
+        System.out.println(T.DFS(n, r));
     }
 }
 
 /*
-3 2
-3 6 9
+5 3
+또는
+33 19
  */

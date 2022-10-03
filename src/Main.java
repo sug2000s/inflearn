b9f111bb13caf2813a267028ab9877dc60368623
CNS\sug2000s <sug2000s@gmail.com>
2022-10-03 21:23:53 +0900

    8-6. 순열 구하기_221003

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import java.util.*;
 class Main{
-    static int[] pm;
+    static int[] pm, ch, arr;
     static int n, m;
     public void DFS(int L){
         if(L==m){
-            System.out.println(Arrays.toString(pm));
+            if(pm[0]==pm[1]){
+                return;
+            }else{
+                System.out.println(Arrays.toString(pm));
+            }
 
         }else{
-            for(int i=1;i<=n; i++){
-                pm[L] = i;
+            for(int i=1; i<=n;i++){
+                pm[L] = arr[i-1];
                 DFS(L+1);
-
             }
         }
 
@@ -20,13 +23,15 @@ class Main{
         Scanner kb = new Scanner(System.in);
         n=kb.nextInt();
         m=kb.nextInt();
+        arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        ch=new int[n];
         pm=new int[m];
         T.DFS(0);
     }
 }
 
 /*
-
 3 2
-
+3 6 9
  */

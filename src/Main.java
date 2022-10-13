8851c05a3088c1a9d60a78c97fd6180a16bf41c6
CNS\sug2000s <sug2000s@gmail.com>
2022-10-13 22:38:53 +0900

    2022/10/13 COS 2번문제 복원

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,48 @@
-import java.util.*;
-class Main{
-    static int[] combi;
-    static int n, m;
-    public void DFS(int L, int s){
-        if(L==m){
-            System.out.println(Arrays.toString(combi));
-        }else{
-            for(int i=s;i<=n;i++){
-                combi[L] = i;
-                DFS(L+1, i+1);
-            }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
+class Main {
+    int solution(int[] dices){
+        int[] chk = new int[dices.length];
+
+        //초기화
+        if(dices[0]%2==0){
+            chk[0] = 1;
+        }else{
+            chk[0] = -1;
         }
+        for(int i=1;i<dices.length;i++){
+            if(dices[i]%2==0){
+                if(chk[i-1]<0){
+                    chk[i] = 1;
+                }else{
+                    chk[i] = chk[i-1] + 1;
+                }
+            }else if(dices[i]%2!=0){
+                if(chk[i-1]>0){
+                    chk[i] = -1;
+                }else{
+                    chk[i] = chk[i-1] - 1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(chk));
+
+
+        return 0;
     }
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        n=kb.nextInt();
-        m=kb.nextInt();
-        combi=new int[m];
-        T.DFS(0, 1);
+
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        int[] dices = {3, 3, 2, 2, 2};
+        int ret1 = sol.solution(dices);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+
     }
 }
-/*
-4 2
- */
